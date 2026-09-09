76589dba1bc13be47e0d81d61c07a9ddece017b0
Tertius van Niekerk <tertius.vanniekerk@protonmail.com>
2026-09-09 07:04:08 +0200

    Updated site content

--- main/.svelte-kit/output/server/chunks/internal.js
+++ main/.svelte-kit/output/server/chunks/internal.js
@@ -2646,7 +2646,7 @@ const options = {
 		<div class="error">
 			<span class="status">` + status + '</span>\n			<div class="message">\n				<h1>' + message + "</h1>\n			</div>\n		</div>\n	</body>\n</html>\n"
   },
-  version_hash: "ys26zb"
+  version_hash: "1ev9z3z"
 };
 async function get_hooks() {
   let handle;
